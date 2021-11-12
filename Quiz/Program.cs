de7486b788c098e498ddf6331b027486544122ea
Wawszyn <93470268+Wawszyn@users.noreply.github.com>
2021-11-12 23:23:31 +0100

    Add files via upload

---
+++ Quiz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quiz/Pytanie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Pytanie
+    {
+        public int Id { get; set; }
+        public int Kategoria { get; set; }
+        public string Tresc { get; set; }
+        public string Odp1 { get; set; }
+        public string Odp2 { get; set; }
+        public string Odp3 { get; set; }
+        public string Odp4 { get; set; }
+    }
+}
